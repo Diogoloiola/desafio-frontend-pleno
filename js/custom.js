b544b340cba2faf2d54b93919861b6b52b291ebf
Diogo Alves <diogalves@gmail.com>
2021-06-17 20:43:22 -0300

    iniciando a busca de informações pelo form

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
 import { app } from './app.js'
+import { form } from './form.js'
 
 $(document).ready(function() {
+    let btnIconSearch = document.querySelector('#search-icon')
+    btnIconSearch.addEventListener('click', () => form.iconSearch())
+
+    let btnSearch = document.querySelector('.icon-form')
+    btnSearch.addEventListener('click', () => form.search())
+
+
     app.getData('http://localhost:3333/chart/tracks')
 });
-
-function searchForTracks() {
-    // You Code Javascript
-}
-
-function goBackInitialTracks() {
-    // You Code Javascript
-}
