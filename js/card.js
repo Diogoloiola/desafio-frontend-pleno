326210ba0c8f8d11642120627f807a99925fc574
Diogo Alves <diogalves@gmail.com>
2021-06-18 09:06:13 -0300

    adicionado icone de play nos cards

--- js/card.js
+++ js/card.js
@@ -29,6 +29,13 @@ let card = {
         for (let i = 0; i < 5; i++) {
             let div = document.createElement('div')
             let imgInfo = document.createElement('img')
+            let span = document.createElement('span')
+            let icon = document.createElement('i')
+
+            icon.classList.add('far')
+            icon.classList.add('fa-play-circle')
+
+            span.appendChild(icon)
 
             div.classList.add('info')
             imgInfo.classList.add('w-100')
@@ -37,6 +44,7 @@ let card = {
             imgInfo.src = obj[i].picture_medium
 
             div.appendChild(imgInfo)
+            div.appendChild(span)
             container.appendChild(div)
 
         }
@@ -49,6 +57,14 @@ let card = {
             let div = document.createElement('div')
             let imgInfo = document.createElement('img')
 
+            let span = document.createElement('span')
+            let icon = document.createElement('i')
+
+            icon.classList.add('far')
+            icon.classList.add('fa-play-circle')
+
+            span.appendChild(icon)
+
             div.classList.add('info')
             imgInfo.classList.add('w-100')
 
@@ -57,7 +73,9 @@ let card = {
             } else {
                 imgInfo.src = obj[i].cover_medium
             }
+
             div.appendChild(imgInfo)
+            div.appendChild(span)
             container.appendChild(div)
 
         }
