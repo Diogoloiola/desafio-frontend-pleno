a14adf1039c7f4d17f95279fdf9ec769cf85b32e
Diogo Alves <diogalves@gmail.com>
2021-06-18 08:26:38 -0300

    adicionada a animação de load, e variaveis renomeadas

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 import { card } from './card.js'
-
 let app = {
     getData(url) {
+        let loadDiv = document.querySelector('.container-load')
+        let main = document.querySelector('#main')
         $.ajax({
             url: url,
             type: 'GET',
-            success: (dados) => {
-                card.renderizeData(dados)
+            beforeSend: function(xhr) {
+                loadDiv.classList.remove('d-none')
+            },
+            success: (data) => {
+                card.renderizeData(data)
+            },
+            complete(data) {
+                loadDiv.classList.add('d-none')
+                main.classList.remove('d-none')
             }
         });
     }
